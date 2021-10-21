27593f525d658f448d86916e653e55d045befd4e
Abbas Manning <60321696+Abbas-Codes@users.noreply.github.com>
2021-10-21 10:55:54 +0200

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,10 @@
+function openMenu() {
+  var menu = document.getElementById("mobile-links");
+  menu.style.display = "block";
+}
+
+function closeMenu() {
+  var menu = document.getElementById("mobile-links");
+  menu.style.display = "none";
+}
+
